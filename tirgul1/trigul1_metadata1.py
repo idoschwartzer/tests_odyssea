415582779f0bdbca74490a09977412f19b95ef69
idoschwartzer <97401114+idoschwartzer@users.noreply.github.com>
2022-07-18 10:20:23 +0300

    Update trigul1_metadata1.py

--- tirgul1/trigul1_metadata1.py
+++ tirgul1/trigul1_metadata1.py
@@ -1,1 +1,15 @@
- # def encode_metadata(file_path, text, meta):
+def print_meta(file, meta):
+    if meta in tags_dict:
+        raise Exception("INFO: Tag is supported")
+        with open(file, 'rb') as image_file:
+
+def build_tags_dict():
+    tags_dict[""] = 0x010e
+    tags_dict["make"]=0x010f
+    tags_dict["model"]=0x0110
+
+
+
+tags_dict = dict()
+build_tags_dict()
+print_meta('img_input\\art.jpg', "Title")
